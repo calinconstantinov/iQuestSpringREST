fe58163764a16068a09972b2ba9849b78ab725ae
Calin Constantinov <calin.constantinov@iquestgroup.com>
2018-03-28 09:24:19 +0300

    small config changes

--- service/src/main/java/com/iquestgroup/demo/service/mapping/converter/ManyConverter.java
+++ service/src/main/java/com/iquestgroup/demo/service/mapping/converter/ManyConverter.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class ManyConverter {
 
   public <T1 extends Model, T2 extends Data> List<T2> convert(Iterable<T1> modelList, Converter<T1, T2> converter) {
-    List<T2> dataList = new ArrayList<T2>(Iterables.size(modelList));
+    List<T2> dataList = new ArrayList<>(Iterables.size(modelList));
     for(T1 model : modelList) {
       T2 data = converter.convertToData(model);
       dataList.add(data);
